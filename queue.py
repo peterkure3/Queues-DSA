a7701d8e0eb483af46232b5a1c8ca54c2949fa88
Peter Kure <84036156+peterkure3@users.noreply.github.com>
2022-08-21 20:02:09 +0300

    Add files via upload

---
+++ queue.py
@@ -0,0 +1,23 @@
+# Initialized empty queue
+
+queue =[]
+
+# Inserting elements into the queue
+queue.append("Subaru")
+queue.append("lexus")
+queue.append("brabus")
+queue.append("dodge")
+queue.append("cadillac")
+
+print("First queue:")
+print(queue)
+
+# Removing elements from the queue
+print("\nElements dequeued from queue:")
+print(queue.pop(0))
+print(queue.pop(0))
+print(queue.pop(0))
+
+print("\nElements left in the queue:")
+print(queue)
+
